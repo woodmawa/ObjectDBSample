2052e05240c9af2ea01397132379d98ed5a55d48
woodmawa <will.woodman@btinternet.com>
2022-04-01 17:44:37 +0100

    basis of session capability

--- src/test/groovy/scripts/deprecatedTestProxyAndClosureAccessForWhereClause.groovy
+++ src/test/groovy/scripts/deprecatedTestProxyAndClosureAccessForWhereClause.groovy
@@ -1,18 +1,23 @@
 package scripts
 
 import groovy.transform.TypeChecked
+import org.codehaus.groovy.runtime.MethodClosure
 
 class DeprecatedDomain {
-    String name = "William"
+    String name = "default name: William"
 
     def someWork () {
         println "\t->Domains, someWork(): $name"
+    }
+
+    String toString() {
+        "DeprecatedDomain ([$name])"
     }
 }
 
 trait DeprecatedGormTrait {
     def where (@DelegatesTo(DeprecatedGormTrait) Closure closure) {
-        println ">>from gorm trait running closure "
+        println ">>from gormTrait.where() running closure "
         Closure clos = closure.clone()
         clos.delegate = this
         def result = clos()
@@ -20,7 +25,7 @@ trait DeprecatedGormTrait {
     }
 
     def and (@DelegatesTo (DeprecatedGormTrait) expr) {
-        println "eval ${(expr)}"
+        println "gromTrait and(): eval ${(expr)}"
         this
     }
 }
@@ -28,31 +33,27 @@ trait DeprecatedGormTrait {
 
 class DeprecatedGormClass {
     def where (@DelegatesTo (DeprecatedGormClass) Closure closure) {  //@DelegatesTo(GormClass)
-        println ">>GormClass.where() running closure "
+        println ">>from GormClass.where() running closure "
         Closure clos = closure.clone()
-        clos.delegate = this
+        //clos.delegate = this
         def result = clos()
         result
     }
 
     def and ( expr) {  //@DelegatesTo (GormClass)
-        println "eval ${expr}"
+        println "GormClass.and(): eval ${expr}"
         this
+    }
+
+    String toString() {
+        "GormClass [@${(Long.toHexString( this.hashCode() ))}]"
     }
 }
 
-
-//get before with traits enhancement
-DeprecatedDomain instance = new DeprecatedDomain()
-
-instance.metaClass.thingy = {-> println "thingy called  "}
-instance.metaClass.val = "any added value"
-
-DeprecatedDomain instance2 = new DeprecatedDomain()
-
-List l = instance.metaClass.methods.collect {it.name}
-
 class DeprecatedDomainProxy extends groovy.util.Proxy {
+
+    //create instance for rehydration purposes
+    static DeprecatedGormClass gorm = new DeprecatedGormClass()
 
     DeprecatedDomainProxy(proxy) {  //@DelegatesTo (Domain)
         List origMethods = proxy.metaClass.methods.collect{it.name}
@@ -73,11 +74,8 @@ class DeprecatedDomainProxy extends groovy.util.Proxy {
             emc.registerInstanceMethod(it)}
         //diff2.each {it.setMetaClass(emc)
             //emc.registerInstanceMethod(it) }  //add in the GormClass metaMethods
-       emc.initialize()
+        emc.initialize()
         adaptee.setMetaClass(emc)
-
-        adaptee.metaClass.where = { DeprecatedGormClass::where }
-        adaptee.metaClass.and = {DeprecatedGormClass::and }
 
         List metaMethods = adaptee.metaClass.methods.collect{it.name}
         metaMethods
@@ -105,8 +103,15 @@ class DeprecatedDomainProxy extends groovy.util.Proxy {
 
     def where (@DelegatesTo (DeprecatedDomain) Closure closure) {  //
         //relay where call to proxy, with Gorm enhancement trait
-        adaptee.where (closure.clone())
-        this
+        Closure work = closure.clone()
+        //rehydrate (delegate, owner, thisOwner)
+        work = work.rehydrate(adaptee, gorm, null)
+        work.setResolveStrategy(Closure.DELEGATE_FIRST) //try adaptee first, then gorm
+
+        println "domainProxy.where(), delegates to $gorm where to run closure "
+        MethodClosure gormWhere = gorm::where as MethodClosure
+        //not required - adapteeWhere.delegate = adaptee
+        gormWhere (work)  //using gorms.where(work)
     }
 
 
@@ -123,12 +128,23 @@ instance2.metaClass.where = (Dummy::where).rehydrate(instance2 , dummy, dummy)  
 */
 //instance2.where { println "instance 2 with borrowed trait method "}
 
+//get before with traits enhancement
+DeprecatedDomain instance = new DeprecatedDomain(name:"instance")
+
+instance.metaClass.thingy = {-> println "thingy called  "}
+instance.metaClass.val = "any added value"
+
+DeprecatedDomain instance2 = new DeprecatedDomain(name:"instance2")
+
+List l = instance.metaClass.methods.collect {it.name}
+instance.thingy()
 
 //enhance Class with some traits
 def EnhancedDomainClass = DeprecatedDomain.withTraits DeprecatedGormTrait
 DeprecatedDomain instance2FromEnhDomainClass = EnhancedDomainClass.newInstance()
+instance2FromEnhDomainClass.name = "inst2 from enhancedDomainClass"
 
-Proxy dp = new DeprecatedDomainProxy(instance)
+groovy.util.Proxy dp = new DeprecatedDomainProxy(instance)
 //instance = dp.newInstance()
 def res =instance.metaClass.respondsTo (DeprecatedDomain, "someWork")
 
@@ -155,14 +171,31 @@ List ldpproxy =  dp.adaptee.getMetaClass().getMethods().collect {it.name}
 dp.adaptee.thingy() // {println "hello in ldpproxy "}
 println "val from dp " + dp.adaptee.val
 
-dp.where {
-    boolean result = name == 'William'
+def whereResult = dp.where {
+    boolean result = name == 'instance'
     someWork()
     thingy()
     println val
-    and (10)
-    println "\tgorm adapted proxy in where closure where name is $name, own:$owner delegate:$delegate"
+    and (10)  //uses gorm class owners and
+    println "\tgorm adapted proxy in dp.where() with closure context name is $name, own:$owner delegate:$delegate"
+    "all done"
 }
+
+println "-> proxies where(Closure) call  returned $whereResult"
+
+println "\n\t now setup dp2 from instance2 and try \n"
+
+DeprecatedDomainProxy dp2 = new DeprecatedDomainProxy(instance2FromEnhDomainClass)
+whereResult = dp2.where {
+    someWork()
+    thingy()    //from adjusted metaClass change on DomainClass
+    println val
+    and (10)  //uses gorm class owners and
+    println "\tgorm adapted proxy in dp2.where() with closure context name is $name, own:$owner delegate:$delegate"
+    "all done"
+}
+
+
 
 
 
