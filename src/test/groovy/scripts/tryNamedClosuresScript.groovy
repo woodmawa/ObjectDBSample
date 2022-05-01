4fabdc09cb980bce2e36d6f5013dd97505a164b6
woodmawa <will.woodman@btinternet.com>
2022-05-01 11:47:40 +0100

    basis of session capability

--- src/test/groovy/scripts/tryNamedClosuresScript.groovy
+++ src/test/groovy/scripts/tryNamedClosuresScript.groovy
@@ -25,10 +25,10 @@ Alt alt2 = new Alt(name:'named alt2')
 Closure alt1Clos = someClos.rehydrate(alt1,alt1, null)
 Closure alt2Clos = someClos.rehydrate(alt2,alt2,null)
 
-print "call of closure with alt1 as context 1 >> "
+print "call of closure with alt1 as context, with 1 >> "
 alt1Clos (1)
 
-print "call of closure with alt2 as context 2 >> "
+print "call of closure with alt2 as context, with 2 >> "
 alt2Clos (2)
 
 /*
