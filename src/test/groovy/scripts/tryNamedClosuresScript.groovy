1bfa8b96dae4852507c1986cb9708afd2ea0e373
woodmawa <will.woodman@btinternet.com>
2022-05-01 11:46:51 +0100

    basis of session capability

--- src/test/groovy/scripts/tryNamedClosuresScript.groovy
+++ src/test/groovy/scripts/tryNamedClosuresScript.groovy
@@ -2,22 +2,15 @@ package scripts
 
 import com.softwood.utilities.NamedClosure
 
-//NamedClosure nc = new NamedClosure ("will", {it -> println "do this closure [$it]"})
-String name = "my Script"
+//store value in script binding
+name = "my Script"
 
 Closure someClos = {it ->
     def thisOwner = owner
-    String nm
-    if (thisOwner.hasProperty ('name')) {
-        //true for Alt class, but not for a script
-        nm = thisOwner.name
-    } else {
-        Binding binding = getBinding()
 
-        nm = "property 'name' not defined for closure context ${owner.getClass()}"
-    }
-    println "'name' was $nm"
-    println "\tclos called with arg [$it] (name:${-> nm}) (owner:$owner)"
+    // will try and resolve on either script binding, or Alt instance owner
+    String nm = "${owner.name}"
+    println "\tclos called with arg [$it] (name:$nm) (owner:$owner)"
 }
 print "direct call of closure from script with 0 >> "
 someClos(0)
@@ -28,10 +21,14 @@ class Alt {
 Alt alt1 = new Alt(name:'named alt1')
 Alt alt2 = new Alt(name:'named alt2')
 
+
 Closure alt1Clos = someClos.rehydrate(alt1,alt1, null)
 Closure alt2Clos = someClos.rehydrate(alt2,alt2,null)
 
+print "call of closure with alt1 as context 1 >> "
 alt1Clos (1)
+
+print "call of closure with alt2 as context 2 >> "
 alt2Clos (2)
 
 /*
